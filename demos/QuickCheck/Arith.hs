a60e163b6b37ba67cc508c366eb68247c9ff7f79
Kevin Charter <kevin.charter@acm.org>
2010-05-27 22:34:41 -0600

    Add some tools to make it easier to manually construct Exps.
    With ToExp type class and a couple of helper functions, you
    can now write
    
     1 <+> 14 <*> 2
    
    in ghci for manual testing.

--- demos/QuickCheck/Arith.hs
+++ demos/QuickCheck/Arith.hs
@@ -7,3 +7,32 @@ data Exp = Const Integer |
 
 data BinOp = Add | Sub | Mul | Div deriving (Eq, Ord, Show)
 
+class ToExp a where
+    toExp :: a -> Exp
+
+instance ToExp Exp where
+    toExp = id
+
+instance ToExp Integer where
+    toExp = Const
+
+infixl 6 <+>, <->
+infixl 7 <*>, </>
+
+(<+>) :: (ToExp a, ToExp b) => a -> b -> Exp
+(<+>) = libBinOp Add
+
+(<->) :: (ToExp a, ToExp b) => a -> b -> Exp
+(<->) = libBinOp Sub
+
+(<*>) :: (ToExp a, ToExp b) => a -> b -> Exp
+(<*>) = libBinOp Mul
+
+(</>) :: (ToExp a, ToExp b) => a -> b -> Exp
+(</>) = libBinOp Div
+
+libBinOp :: (ToExp a, ToExp b) => BinOp -> a -> b -> Exp
+libBinOp = liberalize2 . BinOpCall
+
+liberalize2 :: (ToExp a, ToExp b) => (Exp -> Exp -> Exp) -> a -> b -> Exp
+liberalize2 f x y = f (toExp x) (toExp y)
